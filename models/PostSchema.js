a54b29d44a33ccdaf605c59b97a690623ee25150
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-10-16 20:55:14 -0300

    Criando model de postagens

---
+++ models/PostSchema.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  slug: {
+    type: String,
+    required: true
+  },
+  descripton: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'categories',
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+mongoose.model('posts', PostSchema);
+
+export default PostSchema;
